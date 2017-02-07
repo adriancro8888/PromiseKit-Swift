0a8b9939f905a4233c5f0a62fe98447e329ffaf8
adriancro8888 <adriancro8888@gmail.com>
2017-02-07 19:06:27 +0500

    Implemented new method to fetch weather data using Promisekit fulfill and reject

--- PromiseKitTutorial/WeatherHelper.swift
+++ PromiseKitTutorial/WeatherHelper.swift
@@ -24,83 +24,107 @@
 import Foundation
 import PromiseKit
 
-fileprivate let appID = "<#Enter Your API Key from http://openweathermap.org/appid#>"
+fileprivate let appID = "321af630d5a3d10088c77e00507821e2"
 
 class WeatherHelper {
-  
-  struct Weather {
-    let tempInK: Double
-    let iconName: String
-    let text: String
-    let name: String
     
-    init?(jsonDictionary: [String: Any]) {
-      
-      guard let main =  jsonDictionary["main"] as? [String: Any],
-        let tempInK = main["temp"] as? Double,
-        let weather = (jsonDictionary["weather"] as? [[String: Any]])?.first,
-        let iconName = weather["icon"] as? String,
-        let text = weather["description"] as? String,
-        let name = jsonDictionary["name"] as? String else {
-        print("Error: invalid jsonDictionary! Verify your appID is correct")
-        return nil
-      }
-      self.tempInK = tempInK
-      self.iconName = iconName
-      self.text = text      
-      self.name = name
-    }
-  }
-  
-  func getWeatherTheOldFashionedWay(latitude: Double, longitude: Double, completion: @escaping (Weather?, Error?) -> ()) {
-    
-    assert(appID != "<#Enter Your API Key from http://openweathermap.org/appid#>", "You need to set your API key!")
-    
-    let urlString = "http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(appID)"
-    let url = URL(string: urlString)!
-    let request = URLRequest(url: url)
-    
-    let session = URLSession.shared
-    let dataTask = session.dataTask(with: request) { data, response, error in
-      
-      guard let data = data,
-        let json = (try? JSONSerialization.jsonObject(with: data, options: [])) as? [String: Any],
-        let result = Weather(jsonDictionary: json) else {
-          completion(nil, error)
-          return
-      }
-      
-      completion(result, nil)
-    }
-    dataTask.resume()
-  }  
-  
-  private func saveFile(named: String, data: Data, completion: @escaping (Error?) -> Void) {
-    DispatchQueue.global(qos: .background).async {
-      if let path = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.appendingPathComponent(named+".png") {
-        do {
-          try data.write(to: path)
-          print("Saved image to: " + path.absoluteString)
-          completion(nil)
-        } catch {
-          completion(error)
+    struct Weather {
+        let tempInK: Double
+        let iconName: String
+        let text: String
+        let name: String
+        
+        init?(jsonDictionary: [String: Any]) {
+            
+            guard let main =  jsonDictionary["main"] as? [String: Any],
+                let tempInK = main["temp"] as? Double,
+                let weather = (jsonDictionary["weather"] as? [[String: Any]])?.first,
+                let iconName = weather["icon"] as? String,
+                let text = weather["description"] as? String,
+                let name = jsonDictionary["name"] as? String else {
+                    print("Error: invalid jsonDictionary! Verify your appID is correct")
+                    return nil
+            }
+            self.tempInK = tempInK
+            self.iconName = iconName
+            self.text = text
+            self.name = name
         }
-      }
     }
-  }
-  
-  private func getFile(named: String, completion: @escaping (UIImage?) -> Void) {
-    DispatchQueue.global(qos: .background).async {
-      var image: UIImage?
-      if let path = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.appendingPathComponent(named+".png") {
-        if let data = try? Data(contentsOf: path) {
-          image = UIImage(data: data)
+    
+    func getWeatherTheOldFashionedWay(latitude: Double, longitude: Double, completion: @escaping (Weather?, Error?) -> ()) {
+        
+        assert(appID != "<#Enter Your API Key from http://openweathermap.org/appid#>", "You need to set your API key!")
+        
+        let urlString = "http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(appID)"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request) { data, response, error in
+            
+            guard let data = data,
+                let json = (try? JSONSerialization.jsonObject(with: data, options: [])) as? [String: Any],
+                let result = Weather(jsonDictionary: json) else {
+                    completion(nil, error)
+                    return
+            }
+            
+            completion(result, nil)
         }
-      }
-      DispatchQueue.main.async {
-        completion(image)
-      }
+        dataTask.resume()
     }
-  }
-  
+    
+    func getWeather(latitude:Double,longitude:Double) -> Promise<Weather>{
+        return Promise{ fullfill, reject in
+            let urlString = "http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)" +
+            "&appid=\(appID)"
+            
+            let request = URLRequest(url: URL(string: urlString)!)
+            
+            URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+                if let data = data,
+                    let json = (try? JSONSerialization.jsonObject(with: data, options: [])) as? [String:Any],
+                    let result = Weather(jsonDictionary: json){
+                    fullfill(result)
+                }else if let error = error{
+                    reject(error)
+                }else{
+                    let error = NSError(domain: "PromiseKit", code: 0, userInfo: [NSLocalizedDescriptionKey: "Unknown error"])
+                    reject(error)
+                }
+            }).resume()
+            
+        }
+        
+    }
+    
+    private func saveFile(named: String, data: Data, completion: @escaping (Error?) -> Void) {
+        DispatchQueue.global(qos: .background).async {
+            if let path = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.appendingPathComponent(named+".png") {
+                do {
+                    try data.write(to: path)
+                    print("Saved image to: " + path.absoluteString)
+                    completion(nil)
+                } catch {
+                    completion(error)
+                }
+            }
+        }
+    }
+    
+    private func getFile(named: String, completion: @escaping (UIImage?) -> Void) {
+        DispatchQueue.global(qos: .background).async {
+            var image: UIImage?
+            if let path = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first?.appendingPathComponent(named+".png") {
+                if let data = try? Data(contentsOf: path) {
+                    image = UIImage(data: data)
+                }
+            }
+            DispatchQueue.main.async {
+                completion(image)
+            }
+        }
+    }
+    
 }
